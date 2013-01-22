48e226d742bd988c7c677a90e8927a20f31baeaf
Max Revitt <max.revitt@gmail.com>
2013-01-22 16:14:00 +0000

    more work on event audit lot

--- ILDSS/Ildss/Ildss/Test.cs
+++ ILDSS/Ildss/Ildss/Test.cs
@@ -22,6 +22,8 @@ namespace Ildss
 
             private void btnReadEvents_Click(object sender, EventArgs e)
 {
+               
+
     int i = 0;
     int nEvents = 50000;//Convert.ToInt32(txtNumEvents.Text);
  
@@ -32,18 +34,20 @@ namespace Ildss
 
     foreach (System.Diagnostics.EventLogEntry entry in myEvents.Entries)
     {
-        if (i < nEvents)
+        Console.WriteLine(entry.Message.ToString());
+        /*if (i < nEvents)
         {
             // NEED TO IGNORE desktop.ini files!!!
             // Work out a list of ignored files.
 
             //if (entry.InstanceId == 4656)
             //{
-                foreach (var d in entry.ReplacementStrings)
+                foreach (var d in entry.Message)
                 {
+                    Console.WriteLine(d.ToString());
                     if (File.Exists(d.ToString()) && d.ToString().StartsWith(@"F:\TestDir"))
                     {
-                        Console.WriteLine("data: " + entry.EntryType + " -- " + entry.TimeGenerated + " -- " + d.ToString());
+                        Console.WriteLine("data: " + entry.EntryType + " -- " + entry.TimeGenerated + " -- " + d.ToString() + " -- " + entry.EventID);
                     }
                 }
                 // Increment control for loop, or it will pull the whole log.
@@ -54,7 +58,7 @@ namespace Ildss
         {
             // Break out of loop when i = nEvents.
             break;
-        }
+        }*/
     }
     logTimer.Stop();
     lblTimeProcessed.Text = "Processing took " + logTimer.Elapsed.TotalSeconds.ToString() + " seconds.";
